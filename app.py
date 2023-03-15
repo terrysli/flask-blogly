a3a121b6abd7a5f032aae5829f7dcd46c2b1da6b
Terry Li <terryseanli@gmail.com>
2023-03-15 15:30:51 -0700

    started writing all routes, created all templates

--- app.py
+++ app.py
@@ -2,8 +2,10 @@
 
 import os
 
-from flask import Flask
-from models import connect_db
+from flask import Flask, redirect,  render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
@@ -12,3 +14,15 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
+
+@app.get("/")
+def homepage():
+
+    return redirect
+
+
+
+
+@app.get("/users")
+def show
+return render_template("/users.html", users)
--- models.py
+++ models.py
@@ -1,1 +1,36 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+class User(db.Model):
+    """Class to contain user info"""
+
+    __tablename__ = "users"
+
+    id = db.Column(
+        db.Integer,
+        primary_key = True,
+        autoincrement = True
+    )
+
+    first_name = db.Column(
+        db.String(50),
+        nullable = False
+    )
+
+    last_name = db.Column(
+        db.String(50),
+        nullable = False
+    )
+
+    image_url = db.Column(
+        db.String(100)
+        nullable = True
+    )
+
+    @classmethod
+    def get_all_users(cls):
+        """Returns a list of all User instances"""
+
+        return User.query.all()
